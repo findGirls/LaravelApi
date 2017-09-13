bb7b482b4eb2fa3fe24eb5dfac65c5107775638e
Millyn <9047230@qq.com>
2017-09-13 15:19:37 +0000

    add jwt , add sql tabel

---
+++ database/migrations/2017_09_13_145644_create_photo_tag_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhotoTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photo_tag', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 30);
+            $table->text('intro');
+            $table->tinyInteger('status',false,true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photo_tag');
+    }
+}
