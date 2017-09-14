f439f1e35f142199aebc4ae7286964f2f49d2c46
Millyn <9047230@qq.com>
2017-09-14 21:14:56 +0800

    完成图片增、查功能

--- app/Http/Controllers/Photo/PhotoController.php
+++ app/Http/Controllers/Photo/PhotoController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\Photo;
 
+use Validator;
+use App\Photo;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
@@ -9,12 +11,14 @@ class PhotoController extends Controller
 {
     /**
      * Display a listing of the resource.
-     *
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
-        return "Hello World";
+        empty($request->pageNum) ? $page = 20 : $page = $request->pageNum;
+        $photo = Photo::paginate($page);
+        return $photo;
     }
 
     /**
@@ -25,34 +29,62 @@ class PhotoController extends Controller
     public function create()
     {
         //
+        return "create";
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'url' => 'required|URL|unique:photos|max:255',
+            'source' => 'required|max:255',
+        ]);
+
+        // TODO 这里应该是自定义消息，根据什么错误反馈什么消息。
+        if ($validator->fails()) {
+            return response()->json(['errors' => "请求参数不全或数据已存在，请返回检查", 'status' => 500], 200);
+        }
+
+        $x = new Photo();
+        $x->url = $request->input('url');
+        $x->source = $request->input('source');
+        if (empty($request->input('tag_id'))) {
+            $x->tag_id = 0;
+        } else {
+            $x->tag_id = $request->tag_id;
+        }
+        $x->save();
+        return json_encode($x);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function show($id)
     {
         //
+        $photo = Photo::find($id);
+        if (empty($photo)) {
+            return response()->json([
+                'message' => '没有对应的数据',
+                'status' => '404',
+            ]);
+        }
+        return json_encode($photo);
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -63,8 +95,8 @@ class PhotoController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -75,7 +107,7 @@ class PhotoController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
